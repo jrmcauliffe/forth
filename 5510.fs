375c9ca57b7ca944a3f85cdf1891862c6f4e45fa
Jonathan McAuliffe <jon@birchavenue.org>
2020-03-04 15:56:14 +1100

    Update comments and formatting

--- 5510.fs
+++ 5510.fs
@@ -4,11 +4,11 @@
 $200 constant PBASE
 $1   constant ODDOFFSET
 $20  constant EVENOFFSET
-$02   constant POUT
-$04   constant PDIR
-$06   constant PREN
-$08   constant PDS
-$0A   constant PSEL
+$02  constant POUT
+$04  constant PDIR
+$06  constant PREN
+$08  constant PDS
+$0A  constant PSEL
                         \ SEL  DS REN DIR OUT
 $0 constant INMODE-NR   \   0   0   0   0   X  Input
 $4 constant INMODE-PD   \   0   0   1   0   0  Input with pulldown resistor
@@ -16,29 +16,29 @@ $5 constant INMODE-PU   \   0   0   1   0   1  Input with pulldown resistor
 $2 constant OUTMODE-LS  \   0   0   X   1   X  Output with reduced drive strength
 $9 constant OUTMODE-HS  \   0   1   X   1   1  Output with high drive strength
 
-: io ( port# pin# -- pin ) \ combine port and pin into int
+: io  ( port# pin# -- pin ) \ combine port and pin into int
   swap 8 lshift or 2-foldable ;
-: io# ( pin -- u ) \ convert pin to bit position 
+: io#  ( pin -- u ) \ convert pin to bit position
   $7 and 1-foldable ;
-: io-mask ( pin -- u ) \ convert pin to bit mask
+: io-mask  ( pin -- u ) \ convert pin to bit mask
   1 swap io# lshift 1-foldable ;
-: io-port ( pin -- u ) \ convert pin to port number
+: io-port  ( pin -- u ) \ convert pin to port number
   8 rshift 1-foldable ;
-: io-base ( pin -- addr ) \ convert pin to base address
+: io-base  ( pin -- addr ) \ convert pin to base address
   io-port 1 - 2 /mod EVENOFFSET * swap ODDOFFSET * + PBASE + 1-foldable ;
-: io-split ( pin -- io-mask io-base )
+: io-split  ( pin -- io-mask io-base )
   dup io-mask swap io-base 1-foldable ;
-: io-mode! ( mode pin -- ) \ Set io mode registers for pin using constants
+: io-mode!  ( mode pin -- ) \ Set io mode registers for pin using constants
   swap 12 2 DO 2dup $1 AND 0= if io-split i + cbic! else io-split i + cbis! then shr 2 +loop 2drop ;
-: io@ ( pin -- flag )
+: io@  ( pin -- flag )
   io-split POUT + cbit@ ;
-: io-0! ( pin -- ) \ set pin to low
+: io-0!  ( pin -- ) \ set pin to low
   io-split POUT + cbic! ;
-: io-1! ( pin -- ) \ set pin to low
+: io-1!  ( pin -- ) \ set pin to high
   io-split POUT + cbis! ;
-: io! ( f pin -- ) \ set a pin value
+: io!  ( ? pin -- ) \ if true, set pin high else low
   swap if io-1! else io-0! then ;
-: iox! ( pin -- ) \ set pin to low
+: iox!  ( pin -- ) \ Toggle pin value
   io-split POUT + cxor! ;
 
 : us 0 ?do [ $3C00 , $3C00 , ] loop ;
