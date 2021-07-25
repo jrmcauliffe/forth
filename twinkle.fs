6c5994e18b8d1a8a66d801f0e508ea7ea91201cf
Jonathan McAuliffe <jon@birchavenue.org>
2021-07-25 20:11:26 +1000

    Basic blue/green twinkle with delta trigger

--- twinkle.fs
+++ twinkle.fs
@@ -7,8 +7,9 @@ compiletoflash
 \ ADC
 \res export SYSCFG2 ADCCTL0 ADCCTL1 ADCCTL2 ADCMCTL0 ADCMEM0
 \ TIMERS
-\res export TA2CTL TA2CCTL0 TA2CCTL1 TA2CCR0 TA2CCR1
-
+\res export TA0CTL TA0CCTL0 TA0CCTL1 TA0CCTL2 TA0CCR0 TA0CCR1 TA0CCR2 TA2R TA2CTL TA2CCTL0 TA2CCTL1 TA2CCR0 TA2CCR1
+\ CRC (Random Numbers)
+\res export CRC16DI CRCDIRB CRCINIRES CRCRESR
 #include ms.fs
 #include digital-io.fs
 
@@ -18,29 +19,50 @@ compiletoflash
 1 3 io constant pAnalog
 3 0 io constant pAnalogPower
 100 constant darkcutout
-60 constant timeout
-timeout variable remaining
+300 constant delta
+500 constant timeout
+0 variable remaining
+false variable twinkle
+1024 variable lastsample
 
 : sample \ ( -- u )  Sample pin
-  pAnalogPower io-1!
-  $0012 ADCCTL0 BIS! \ turn on
+  pAnalogPower io-1! \ Power voltage divider
+  $0012 ADCCTL0 BIS! \ Turn on
   $0001 ADCCTL0 BIS! \ Sample
   begin $01 ADCCTL1 BIT@ not until \ Wait until busy flag cleared
   ADCMEM0 @
-  $0012 ADCCTL0 BIC! \ turn off
-  pAnalogPower io-0!
+  $0012 ADCCTL0 BIC! \ Turn off
+  pAnalogPower io-0! \ Power off voltage divider
+;
+
+: rand ( -- u ) \ Return psuedorandom number based on 10kHz timer feed through crc
+  TA2R @ CRC16DI ! CRCINIRES c@
+  1 rshift dup *            \ scale down and square for cheap gamma
 ;
 
 : tick-irq-handler
   \ If on, then decrement timer
-  pGreen io@ if remaining @ 1- remaining ! then
+  twinkle @ if
+    remaining @ 1- remaining !
+    rand dup TA0CCR1 !
+    TA0CCR2 !
+  else
+    0 TA0CCR1 !
+    0 TA0CCR2 !
+  then \ TWINKLE!
   \ if timed out reset
-  remaining @ 0 = if timeout remaining ! then
-  \ Only resample if reset
-  remaining @ timeout = if
-    sample darkcutout < if pGreen io-1! else pGreen io-0! then
+  remaining @ 0 = if
+    false twinkle !
+    sample dup dup darkcutout < swap lastsample @ swap - delta > and if
+      true twinkle !
+      timeout remaining !
+    else
+      false twinkle !
+    then
+    lastsample !
   then
 ;
+
 
 : myinit \ ( -- )
   \ Configure ADC
@@ -49,16 +71,32 @@ timeout variable remaining
   $0014 ADCCTL2 !       \ 10 bit Slow sample rate
   $0003 ADCMCTL0 !      \ VCC + VSS, A3
   $0008 SYSCFG2 BIS!    \ Set P1.3 to Analog A3 input
+  sample lastsample !   \ Set baseline light level
+
+  \ Configure CRC for psuedo random numbers (Good enough for twinkles anyhow)
+  \ TODO switch to temp adc based seed
+  $FFFF CRCINIRES !
 
   \ Configure IO
-  OUTMODE-HS pBlue io-mode!        \ Blue LED
-  OUTMODE-HS pGreen io-mode!       \ Green LED
-  OUTMODE-LS pAnalogPower io-mode! \ Powers LDR voltage divider
-  ANALOGMODE pAnalog io-mode!      \ Reads LDR voltage divider
+  OUTMODE-SP1 pBlue io-mode!        \ Blue LED
+  OUTMODE-SP1 pGreen io-mode!       \ Green LED
+  OUTMODE-LS  pAnalogPower io-mode! \ Powers LDR voltage divider
+  ANALOGMODE  pAnalog io-mode!      \ Reads LDR voltage divider
 
   \ Configure Timers
+
+  \ TA0 - LED PWM driver
+  $210  TA0CTL !      \ SMCLK/1 up mode interrupts not enabled
+  $FFFF TA0CCR0 !     \ Set CCR0 for desired led refresh rate
+  $0080 TA0CCTL0 !    \ CCI1B / set\reset mode / interrupts disabled
+  $00E0 TA0CCTL1 ! \
+  $00E0 TA0CCTL2 ! \ TODO workout optimal timings / implement Red
+  $0000 TA0CCR1 !
+  $0000 TA0CCR2 !
+
+  \ TA2 - One interrupt per second
   $0110 TA2CTL !   \ ACLK source, Up mode interrupts
-  $7FFF TA2CCR0 !  \ 1 second from ACLK
+  $7FF TA2CCR0 !  \  less than 1 sec from ACLK
   $0090 TA2CCTL0 ! \ Toggle mode, interrupts enabled
 
   \ Per second interrupt
